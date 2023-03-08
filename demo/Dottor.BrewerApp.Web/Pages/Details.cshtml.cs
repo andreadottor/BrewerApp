ea06a304a635793aa9b8c95e6e87b3a04888fa0b
Andrea Dottor <andrea@dottor.net>
2023-03-08 11:26:47 +0100

    Add project for live demo

---
+++ demo/Dottor.BrewerApp.Web/Pages/Details.cshtml.cs
@@ -0,0 +1,39 @@
+using Dottor.BrewerApp.Common.Models;
+using Dottor.BrewerApp.Common.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dottor.BrewerApp.Web.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IBrewerService _brewerService;
+        private readonly ILogger<DetailsModel> _logger;
+
+        public Beer? Beer { get; private set; }
+
+        public DetailsModel(IBrewerService brewerService, ILogger<DetailsModel> logger)
+        {
+            _brewerService = brewerService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            try
+            {
+                Beer = await _brewerService.GetBeerAsync(id);
+                if(Beer is null)
+                {
+                    Response.StatusCode = 404;
+                }
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error on retrieve a beer with id '{beerId}'.", id);
+                return RedirectToPage("Error");
+            }
+        }
+    }
+}
---
+++ demo/Dottor.BrewerApp.Web/Program.cs
@@ -0,0 +1,47 @@
+using Dottor.BrewerApp.Common;
+using Dottor.BrewerApp.Web.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddBrewerServices();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = "BrewerApp API",
+        Version = "v1"
+    });
+    options.EnableAnnotations();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapBeersEndpoint();
+app.MapRazorPages();
+app.MapBlazorHub();
+
+app.Run();
