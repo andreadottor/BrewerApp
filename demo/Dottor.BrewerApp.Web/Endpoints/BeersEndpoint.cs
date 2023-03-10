eb662398da895df781569870b0efb5ac6d2d3056
Andrea Dottor <andrea@dottor.net>
2023-03-10 12:28:13 +0100

    namespace indentation

--- demo/Dottor.BrewerApp.Web/Endpoints/BeersEndpoint.cs
+++ demo/Dottor.BrewerApp.Web/Endpoints/BeersEndpoint.cs
@@ -1,99 +1,98 @@
-﻿namespace Dottor.BrewerApp.Web.Endpoints
+﻿namespace Dottor.BrewerApp.Web.Endpoints;
+
+using Dottor.BrewerApp.Common.Models;
+using Dottor.BrewerApp.Common.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+public static class BeersEndpoint
 {
-    using Dottor.BrewerApp.Common.Models;
-    using Dottor.BrewerApp.Common.Services;
-    using Swashbuckle.AspNetCore.Annotations;
 
-    public static class BeersEndpoint
+    public static IEndpointRouteBuilder MapBeersEndpoint(this IEndpointRouteBuilder endpoints)
     {
+        endpoints.MapGet("/api/v1/beers", GetBeersAsync)
+                    .WithName("Beers")
+                    .WithTags("Public")
+                    .Produces<IEnumerable<Beer>>(StatusCodes.Status200OK)
+                    .WithMetadata(new SwaggerOperationAttribute(
+                            summary: "GetBeersAsync",
+                            description: "Get beer list"));
 
-        public static IEndpointRouteBuilder MapBeersEndpoint(this IEndpointRouteBuilder endpoints)
+        endpoints.MapGet("/api/v1/beers/{id}", GetBeerByIdAsync)
+                    .WithName("BeerById")
+                    .WithTags("Public")
+                    .Produces<Beer>(StatusCodes.Status200OK)
+                    .Produces(StatusCodes.Status404NotFound)
+                    .WithMetadata(new SwaggerOperationAttribute(
+                            summary: "GetBeerByIdAsync",
+                            description: "Get beer by id"));
+
+        endpoints.MapGet("/api/v1/beers/random", GetRandomBeerAsync)
+                    .WithName("RandomBeer")
+                    .WithTags("Public")
+                    .Produces<Beer>(StatusCodes.Status200OK)
+                    .WithMetadata(new SwaggerOperationAttribute(
+                            summary: "GetRandomBeerAsync",
+                            description: "Get a random beer"));
+
+        return endpoints;
+    }
+
+    public static async Task<IResult> GetBeersAsync(
+                                    IBrewerService brewerService,
+                                    ILoggerFactory loggerFactory)
+    {
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
+
+        try
         {
-            endpoints.MapGet("/api/v1/beers", GetBeersAsync)
-                        .WithName("Beers")
-                        .WithTags("Public")
-                        .Produces<IEnumerable<Beer>>(StatusCodes.Status200OK)
-                        .WithMetadata(new SwaggerOperationAttribute(
-                                summary: "GetBeersAsync",
-                                description: "Get beer list"));
-
-            endpoints.MapGet("/api/v1/beers/{id}", GetBeerByIdAsync)
-                        .WithName("BeerById")
-                        .WithTags("Public")
-                        .Produces<Beer>(StatusCodes.Status200OK)
-                        .Produces(StatusCodes.Status404NotFound)
-                        .WithMetadata(new SwaggerOperationAttribute(
-                                summary: "GetBeerByIdAsync",
-                                description: "Get beer by id"));
-
-            endpoints.MapGet("/api/v1/beers/random", GetRandomBeerAsync)
-                        .WithName("RandomBeer")
-                        .WithTags("Public")
-                        .Produces<Beer>(StatusCodes.Status200OK)
-                        .WithMetadata(new SwaggerOperationAttribute(
-                                summary: "GetRandomBeerAsync",
-                                description: "Get a random beer"));
-
-            return endpoints;
+            var list = await brewerService.GetBeersAsync();
+            return Results.Ok(list);
         }
-
-        public static async Task<IResult> GetBeersAsync(
-                                        IBrewerService brewerService,
-                                        ILoggerFactory loggerFactory)
+        catch (Exception ex)
         {
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
-
-            try
-            {
-                var list = await brewerService.GetBeersAsync();
-                return Results.Ok(list);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on retrieve beers list.");
-                return Results.Problem(ex.Message, title: "Error on Beers API");
-            }
+            logger.LogError(ex, "Error on retrieve beers list.");
+            return Results.Problem(ex.Message, title: "Error on Beers API");
         }
+    }
 
-        public static async Task<IResult> GetBeerByIdAsync(
-                                        int id,
-                                        IBrewerService brewerService,
-                                        ILoggerFactory loggerFactory)
+    public static async Task<IResult> GetBeerByIdAsync(
+                                    int id,
+                                    IBrewerService brewerService,
+                                    ILoggerFactory loggerFactory)
+    {
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
+
+        try
         {
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
+            var beer = await brewerService.GetBeerAsync(id);
+            if (beer is null)
+                return Results.NotFound();
 
-            try
-            {
-                var beer = await brewerService.GetBeerAsync(id);
-                if (beer is null)
-                    return Results.NotFound();
-
-                return Results.Ok(beer);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on retrieve beers list.");
-                return Results.Problem(ex.Message, title: "Error on Beers API");
-            }
+            return Results.Ok(beer);
         }
-
-        public static async Task<IResult> GetRandomBeerAsync(
-                                IBrewerService brewerService,
-                                ILoggerFactory loggerFactory)
+        catch (Exception ex)
         {
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
+            logger.LogError(ex, "Error on retrieve beers list.");
+            return Results.Problem(ex.Message, title: "Error on Beers API");
+        }
+    }
 
-            try
-            {
-                var beer = await brewerService.GetRandomBeerAsync();
+    public static async Task<IResult> GetRandomBeerAsync(
+                            IBrewerService brewerService,
+                            ILoggerFactory loggerFactory)
+    {
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
 
-                return Results.Ok(beer);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on retrieve beers list.");
-                return Results.Problem(ex.Message, title: "Error on Beers API");
-            }
+        try
+        {
+            var beer = await brewerService.GetRandomBeerAsync();
+
+            return Results.Ok(beer);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error on retrieve beers list.");
+            return Results.Problem(ex.Message, title: "Error on Beers API");
         }
     }
 }
--- dotnet7/Dottor.BrewerApp.Web/Endpoints/BeersEndpoint.cs
+++ dotnet7/Dottor.BrewerApp.Web/Endpoints/BeersEndpoint.cs
@@ -1,126 +1,125 @@
-﻿namespace Dottor.BrewerApp.Web.Endpoints
+﻿namespace Dottor.BrewerApp.Web.Endpoints;
+
+using Dottor.BrewerApp.Common.Models;
+using Dottor.BrewerApp.Common.Services;
+using Dottor.BrewerApp.Web.Extensions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Logging;
+
+public static class BeersEndpoint
 {
-    using Dottor.BrewerApp.Common.Models;
-    using Dottor.BrewerApp.Common.Services;
-    using Dottor.BrewerApp.Web.Extensions;
-    using Microsoft.AspNetCore.Http.HttpResults;
-    using Microsoft.Extensions.Logging;
-
-    public static class BeersEndpoint
+    public static RouteGroupBuilder MapBeersEndpoint(this IEndpointRouteBuilder endpoints)
     {
-        public static RouteGroupBuilder MapBeersEndpoint(this IEndpointRouteBuilder endpoints)
+        var loggerFactory = endpoints.ServiceProvider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
+
+        var group = endpoints.MapGroup("/api/v1/beers");
+        group.WithTags("Public");
+        group.WithOpenApi();
+        group.RequirePerIPAddressRateLimit();
+        group.AddEndpointFilter(async (context, next) =>
         {
-            var loggerFactory = endpoints.ServiceProvider.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
+            logger.LogInformation("Before first filter");
+            var result = await next(context);
+            logger.LogInformation("After first filter");
+            return result;
+        });
 
-            var group = endpoints.MapGroup("/api/v1/beers");
-            group.WithTags("Public");
-            group.WithOpenApi();
-            group.RequirePerIPAddressRateLimit();
-            group.AddEndpointFilter(async (context, next) =>
-            {
-                logger.LogInformation("Before first filter");
-                var result = await next(context);
-                logger.LogInformation("After first filter");
-                return result;
-            });
+        group.MapGet("/", GetBeersAsync)
+                    .WithName("Beers") // set the OpenAPI OperationId
+                    .WithSummary("GetBeersAsync")
+                    .WithDescription("Get beer list");
 
-            group.MapGet("/", GetBeersAsync)
-                        .WithName("Beers") // set the OpenAPI OperationId
-                        .WithSummary("GetBeersAsync")
-                        .WithDescription("Get beer list");
+        group.MapGet("/{id}", GetBeerByIdAsync)
+                    .WithOpenApi(generatedOperation =>
+                    {
+                        generatedOperation.OperationId = "BeerById";
+                        generatedOperation.Summary = "GetBeerByIdAsync";
+                        generatedOperation.Description = "Get beer by id";
 
-            group.MapGet("/{id}", GetBeerByIdAsync)
-                        .WithOpenApi(generatedOperation =>
-                        {
-                            generatedOperation.OperationId = "BeerById";
-                            generatedOperation.Summary = "GetBeerByIdAsync";
-                            generatedOperation.Description = "Get beer by id";
+                        var idParameter = generatedOperation.Parameters[0];
+                        idParameter.Description = "The ID of the beer.";
 
-                            var idParameter = generatedOperation.Parameters[0];
-                            idParameter.Description = "The ID of the beer.";
+                        return generatedOperation;
+                    });
 
-                            return generatedOperation;
-                        });
+        group.MapGet("/random", GetRandomBeerAsync)
+                    .WithName("RandomBeer")
+                    .WithSummary("GetRandomBeerAsync")
+                    .WithDescription("Get a random beer");
 
-            group.MapGet("/random", GetRandomBeerAsync)
-                        .WithName("RandomBeer")
-                        .WithSummary("GetRandomBeerAsync")
-                        .WithDescription("Get a random beer");
+        group.MapPost("/", InsertBeersAsync)
+                    .WithName("InsertBeer")
+                    .WithSummary("InserBeersAsync")
+                    .WithDescription("Insert new beer");
 
-            group.MapPost("/", InsertBeersAsync)
-                        .WithName("InsertBeer")
-                        .WithSummary("InserBeersAsync")
-                        .WithDescription("Insert new beer");
+        return group;
+    }
 
-            return group;
-        }
+    public static async Task<Results<Ok<IEnumerable<Beer>>, ProblemHttpResult>> GetBeersAsync(
+                                        IBrewerService brewerService,
+                                        ILoggerFactory loggerFactory)
+    {
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
 
-        public static async Task<Results<Ok<IEnumerable<Beer>>, ProblemHttpResult>> GetBeersAsync(
-                                            IBrewerService brewerService,
-                                            ILoggerFactory loggerFactory)
+        try
         {
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
-
-            try
-            {
-                var list = await brewerService.GetBeersAsync();
-                return TypedResults.Ok(list);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on retrieve beers list.");
-                return TypedResults.Problem(ex.Message, title: "Error on Beers API");
-            }
+            var list = await brewerService.GetBeersAsync();
+            return TypedResults.Ok(list);
         }
-
-        public static async Task<Results<Ok<Beer>, NotFound, ProblemHttpResult>> GetBeerByIdAsync(
-                                            int id,
-                                            IBrewerService brewerService,
-                                            ILoggerFactory loggerFactory)
+        catch (Exception ex)
         {
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
-
-            try
-            {
-                var beer = await brewerService.GetBeerAsync(id);
-                if (beer is null)
-                    return TypedResults.NotFound();
-
-                return TypedResults.Ok(beer);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on retrieve beers list.");
-                return TypedResults.Problem(ex.Message, title: "Error on Beers API");
-            }
+            logger.LogError(ex, "Error on retrieve beers list.");
+            return TypedResults.Problem(ex.Message, title: "Error on Beers API");
         }
+    }
 
-        public static async Task<Results<Ok<Beer>, ProblemHttpResult>> GetRandomBeerAsync(
-                                            IBrewerService brewerService,
-                                            ILoggerFactory loggerFactory)
+    public static async Task<Results<Ok<Beer>, NotFound, ProblemHttpResult>> GetBeerByIdAsync(
+                                        int id,
+                                        IBrewerService brewerService,
+                                        ILoggerFactory loggerFactory)
+    {
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
+
+        try
         {
-            var logger = loggerFactory.CreateLogger("BeersEndpoint");
+            var beer = await brewerService.GetBeerAsync(id);
+            if (beer is null)
+                return TypedResults.NotFound();
 
-            try
-            {
-                var beer = await brewerService.GetRandomBeerAsync();
-
-                return TypedResults.Ok(beer);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error on retrieve beers list.");
-                return TypedResults.Problem(ex.Message, title: "Error on Beers API");
-            }
+            return TypedResults.Ok(beer);
         }
-        public static async Task<Results<Created<Beer>, BadRequest, ProblemHttpResult>> InsertBeersAsync(Beer beer)
+        catch (Exception ex)
         {
-            // Fake implementation
-            //
-            beer.Id = 123;
-
-            return TypedResults.Created($"/api/v1/beers/{beer.Id}", beer);
+            logger.LogError(ex, "Error on retrieve beers list.");
+            return TypedResults.Problem(ex.Message, title: "Error on Beers API");
         }
+    }
+
+    public static async Task<Results<Ok<Beer>, ProblemHttpResult>> GetRandomBeerAsync(
+                                        IBrewerService brewerService,
+                                        ILoggerFactory loggerFactory)
+    {
+        var logger = loggerFactory.CreateLogger("BeersEndpoint");
+
+        try
+        {
+            var beer = await brewerService.GetRandomBeerAsync();
+
+            return TypedResults.Ok(beer);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error on retrieve beers list.");
+            return TypedResults.Problem(ex.Message, title: "Error on Beers API");
+        }
+    }
+    public static async Task<Results<Created<Beer>, BadRequest, ProblemHttpResult>> InsertBeersAsync(Beer beer)
+    {
+        // Fake implementation
+        //
+        beer.Id = 123;
+
+        return TypedResults.Created($"/api/v1/beers/{beer.Id}", beer);
     }
 }
