74a8c9a31e279ba30994b2a57fe5f1afebfb3cce
Andrea Dottor <andrea@dottor.net>
2023-05-03 09:46:57 +0200

    Add details page (ssr)

--- dotnet8-ssr/Dottor.BrewerApp.Web/Program.cs
+++ dotnet8-ssr/Dottor.BrewerApp.Web/Program.cs
@@ -3,18 +3,20 @@ using Dottor.BrewerApp.Common;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.Configure<KestrelServerOptions>(options =>
 {
     options.AllowSynchronousIO = true;
 });
-builder.Services.AddBrewerServices();
-builder.Services.AddRazorComponents();
 
+builder.Services.AddBrewerServices();
+
+builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapRazorComponents<MainLayout>();
